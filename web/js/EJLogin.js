582227f9430d4560ce5804d4fd4c7458418354ff
eddiedb6 <eddiedb6@gmail.com>
2018-01-09 13:45:12 +0800

    Login under https initial

--- web/js/EJLogin.js
+++ web/js/EJLogin.js
@@ -1,74 +1,35 @@
 function EJLogin(uidUsername, uidPassword) {
-    var getTokenRequest = W3CreateAPI("aidToken");
-    if (getTokenRequest == "") {
+    var username = W3GetUIValue(uidUsername);
+    var password = W3GetUIValue(uidPassword);
+	
+    var loginRequest = W3CreateAPI("aidLogin", username, password);
+    if (loginRequest == "") {
+	alert("Create login request failed!");
 	return;
     }
 
-    var loginFunc = function() {
-	var username = W3GetUIValue(uidUsername);
-	var password = W3GetUIValue(uidPassword);
-	
-	var loginRequest = W3CreateAPI("aidLogin", username, password);
-	if (loginRequest == "") {
-	    return;
-	}
-
-	// Step 3: Do login
-	W3CallAPIAsync(loginRequest, function(data, status) {
-	    W3LogDebug(data);
-	    W3LogDebug(status);
-	
-	    var result = eval("(" + data + ")");
-	    if (result[w3ApiResultStatus] != w3ApiResultSuccessful) {
-		W3LogWarning("Login failed")
-		return;
-	    }
-
-	    var apiDef = W3GetAPIDef("aidLogin");
-	    var session = result[w3ApiResultData][apiDef[w3ApiResult][w3ApiResultData][0][w3ApiDataValue]];
-	    W3LogInfo("Get session: " + session);
-	    W3LogInfo("Login successfully");
-	});
-    }
-    
-    var sendEncryptionFunc = function (encryption) {
-	var sendEncryptionRequest = W3CreateAPI("aidEncryption", encryption);
-	if (sendEncryptionRequest == "") {
-	    return;
-	}
-
-	// Step 2: Generate client encryption which encrypt by server token and send back to server
-	W3CallAPIAsync(sendEncryptionRequest, function(data, status) {
-	    W3LogDebug(data);
-	    W3LogDebug(status);
-
-	    var result = eval("(" + data + ")");
-	    if (result[w3ApiResultStatus] != w3ApiResultSuccessful) {
-		W3LogWarning("Send encryption failed")
-		return;
-	    }
-
-	    loginFunc();
-	});
-    };
-
-    // Step 1: Get token from sever
-    W3CallAPIAsync(getTokenRequest, function(data, status) {
+    W3CallAPIAsync(loginRequest, function(data, status) {
 	W3LogDebug(data);
 	W3LogDebug(status);
 	
-	var tokenResult = eval("(" + data + ")");
-	if (tokenResult[w3ApiResultStatus] != w3ApiResultSuccessful) {
-	    W3LogWarning("Get token failed")
+	var result = eval("(" + data + ")");
+	if (result[w3ApiResultStatus] != w3ApiResultSuccessful) {
+	    W3LogWarning("Login failed!");
+	    alert("Login failed!");
 	    return;
 	}
 
-	var apiDef = W3GetAPIDef("aidToken");
-	var token = tokenResult[w3ApiResultData][apiDef[w3ApiResult][w3ApiResultData][0][w3ApiDataValue]];
-	W3LogInfo("Get token: " + token);
+	var apiDef = W3GetAPIDef("aidLogin");
+	var session = result[w3ApiResultData][apiDef[w3ApiResult][w3ApiResultData][0][w3ApiDataValue]];
+	W3LogInfo("Get session: " + session);
+	W3LogInfo("Login successfully");
 
-	// TODO
-	var encryption = "def456-"
-	sendEncryptionFunc(encryption);
+	// Redirect to finance page
+	var pageRequest = W3CreateAPI("aidPage", "uidPageFinance");
+	if (pageRequest == "") {
+	    alert("Create page request failed!");
+	    return;
+	}
+	location.href = pageRequest;
     });
 }
