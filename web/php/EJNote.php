ff5df6575e19ead933647d820b3aa016b906f56d
eddiedb6 <tiny.fox@163.com>
2018-03-02 12:38:47 +0800

    Create note tab dynamically

--- web/php/EJNote.php
+++ web/php/EJNote.php
@@ -1,7 +1,34 @@
 <?php
 
 function EJCreateNoteTab() {
-    return W3CreateUI("uidDebugContent");
+    $uid = "uidNoteTab";
+    $uiDef = array(
+        w3PropType => w3TypeTab,
+        w3PropCSS => array (
+            "border-style" => "solid",
+            "border-width" => "1px",
+            "background-color" => "white"
+        ),
+        w3PropSubUI => array()
+    );
+    
+    $sql = "select SID from notetag";
+    EJReadTable($sql, function ($row) use (&$uiDef, $uid) {
+        $uiLabel = array(
+            w3PropType => w3TypeLabel,
+            w3PropClass => "cidLRPadding",
+            w3PropString => $row["SID"]
+        );
+
+        $uiPanel = array (
+            w3PropType => w3TypePanel,
+            w3PropSubUI => array("uidDebugContent")
+        );
+
+        array_push($uiDef, array($uiLabel, $uiPanel));
+    });
+
+    return W3CreateUI($uid, $uiDef);
 }
 
 function EJSetNote() {
