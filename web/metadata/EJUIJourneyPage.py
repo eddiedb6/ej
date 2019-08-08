219daac07cbccb0c587c01f5a5546cd396c284e5
ed <eddiedb6@gmail.com>
2019-08-08 17:04:20 +0800

    Introduce journey page

--- web/metadata/EJUIJourneyPage.py
+++ web/metadata/EJUIJourneyPage.py
@@ -13,7 +13,7 @@
         W3Const.w3PropMap: {
             W3Const.w3AttrMapHandler: "EJMapHandler(w3PlaceHolder_1)",
             W3Const.w3AttrMapLocation: "39.918794, 116.398568",
-            W3Const.w3AttrMapKey: "AixlydPEmOi786Yys1IYY5u36B-z1HdoglcnrS7HsohUY5bZorHmJlKBpNryPeM3"
+            W3Const.w3AttrMapKey: ""
         },
         W3Const.w3PropCSS: {
             "border": "2px solid",
